8428dfef9d62a18f5aebd1c6fc6a9632a1f956c9
Amand4May <ahtaldananda18@gmail.com>
2026-08-08 18:51:20 -0300

    chore: initial project setup with React, Vite, and Tailwind v4

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-adv-marinho text-adv-branco min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl">Sequeira d'Arrochella Advocacia - Consultoria, compliance, assessoria e advocacia</h1>
+    </div>
+  )
+}
+
+export default App
